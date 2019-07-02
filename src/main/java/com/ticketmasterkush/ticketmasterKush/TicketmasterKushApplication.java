04ab259d835cce221f1f8a71506d080697f68b87
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-07-02 09:14:35 -0700

    first commit

---
+++ src/main/java/com/ticketmasterkush/ticketmasterKush/TicketmasterKushApplication.java
@@ -0,0 +1,13 @@
+package com.ticketmasterkush.ticketmasterKush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterKushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterKushApplication.class, args);
+	}
+
+}
