8a3a814536381563060922650da7e1a9fffe4e50
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-07-22 13:00:25 -0700

    send email, text and queue to trigger lambda works

--- src/main/java/com/ticketmasterkush/ticketmasterKush/JsonController.java
+++ src/main/java/com/ticketmasterkush/ticketmasterKush/JsonController.java
@@ -1,5 +1,11 @@
 package com.ticketmasterkush.ticketmasterKush;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.MessageAttributeValue;
 import com.ticketmasterkush.ticketmasterKush.database.Task;
 import com.ticketmasterkush.ticketmasterKush.database.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +15,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+
+
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+
 
 @RestController
 @CrossOrigin
@@ -56,10 +65,12 @@ public class JsonController {
         Task t = taskRepository.findById(id).get();
         if(t.getStatus().equals("Available")){
           t.setStatus("Assigned");
+            helperSendSMS();
         } else if(t.getStatus().equals("Assigned")){
             t.setStatus("Accepted");
         } else if(t.getStatus().equals("Accepted")){
             t.setStatus("Finished");
+            helperSendEmail();
         } else if(t.getStatus()== null) {
             t.setStatus("Available");
         }
@@ -99,4 +110,75 @@ public class JsonController {
         Task t = taskRepository.findById(id).get();
         return t;
     }
+
+    //    https://docs.aws.amazon.com/sns/latest/dg/sms_publish-to-phone.html
+    public void helperSendSMS(){
+        AmazonSNSClient snsClient = new AmazonSNSClient();
+        String message = "Kush has assigned task";
+        String phoneNumber = "+13344324282";
+        Map<String, MessageAttributeValue> smsAttributes =
+                new HashMap<String, MessageAttributeValue>();
+        //<set SMS attributes>
+        PublishResult result = snsClient.publish(new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(phoneNumber)
+                .withMessageAttributes(smsAttributes));
+    }
+
+    public void helperSendEmail(){
+        // Replace sender@example.com with your "From" address.
+        // This address must be verified with Amazon SES.
+        String FROM = "kushshrestha01@gmail.com";
+
+        // Replace recipient@example.com with a "To" address. If your account
+        // is still in the sandbox, this address must be verified.
+        String TO = "kush_shrestha01@yahoo.com";
+
+        // The configuration set to use for this email. If you do not want to use a
+        // configuration set, comment the following variable and the
+        // .withConfigurationSetName(CONFIGSET); argument below.
+//        String CONFIGSET = "ConfigSet";
+
+        // The subject line for the email.
+        String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+
+        // The HTML body for the email.
+        String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+                + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+                + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+                + "AWS SDK for Java</a>";
+
+        // The email body for recipients with non-HTML email clients.
+        String TEXTBODY = "This email was sent through Amazon SES "
+                + "using the AWS SDK for Java.";
+
+
+            try {
+                AmazonSimpleEmailService client =
+                        AmazonSimpleEmailServiceClientBuilder.standard()
+                                // Replace US_WEST_2 with the AWS Region you're using for
+                                // Amazon SES.
+                                .withRegion(Regions.US_EAST_1).build();
+                SendEmailRequest request = new SendEmailRequest()
+                        .withDestination(
+                                new Destination().withToAddresses(TO))
+                        .withMessage(new Message()
+                                .withBody(new Body()
+                                        .withHtml(new Content()
+                                                .withCharset("UTF-8").withData(HTMLBODY))
+                                        .withText(new Content()
+                                                .withCharset("UTF-8").withData(TEXTBODY)))
+                                .withSubject(new Content()
+                                        .withCharset("UTF-8").withData(SUBJECT)))
+                        .withSource(FROM);
+                        // Comment or remove the next line if you are not using a
+                        // configuration set
+//                        .withConfigurationSetName(CONFIGSET);
+                client.sendEmail(request);
+                System.out.println("Email sent!");
+            } catch (Exception ex) {
+                System.out.println("The email was not sent. Error message: "
+                        + ex.getMessage());
+            }
+        }
 }
